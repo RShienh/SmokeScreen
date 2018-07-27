ed1345a3b3f99184bbb55925b9010f91cc320984
RZeus <rajbirsinghshienh@gmail.com>
2018-07-27 15:36:00 -0400

    MainActivity View Framework

--- app/src/main/java/com/shienh/rajbir/smokescreen/MainActivity.java
+++ app/src/main/java/com/shienh/rajbir/smokescreen/MainActivity.java
@@ -1,5 +1,7 @@
 package com.shienh.rajbir.smokescreen;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,13 +9,16 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ProgressBar;
+import android.widget.DatePicker;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,11 +26,16 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
+    private static final String TAG = "MAIN_ACTIVITY";
+    protected int mProgressStatus = 0;
     private RelativeLayout mRelative;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
@@ -34,11 +44,16 @@ public class MainActivity extends AppCompatActivity {
     private int CPD = 0, CPP = 0;
     private float YOS = 0, PPP = 0;
     private String CUR = null;
-
-    private TextView mTextProgress;
-    private ProgressBar mProgressBar;
-    private int ProgressStatus = 0;
+    private TextView mTextProgress, mDayText, mSmokeFreeSince, mMoneySaved, mNumberofDays, mLifeRegained, mCigNotSmoked, mTotalCigSmoked, mMoneyWasted, mLifeLost;
+    private CircularProgressBar mProgressBar;
     private Handler handler = new Handler();
+
+    private int mDay, mMonth, mYear, mHour, mMinute;
+    private int mDayFinal = 0, mMonthFinal = 0, mYearFinal = 0, mHourFinal = 0, mMinuteFinal = 0;
+    private SharedPreferences sharedPreferences;
+
+    private Long mTimeStamp;
+    private int dayNumber = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,30 +67,77 @@ public class MainActivity extends AppCompatActivity {
         loadUserProfile();
         mRelative = findViewById(R.id.MainRelative);
 
-        mTextProgress = findViewById(R.id.txtProgress);
-        mProgressBar = findViewById(R.id.xMainProgressBar);
+        mNumberofDays = findViewById(R.id.xNumberOfDays);
 
+        mTextProgress = findViewById(R.id.txtProgress);
+        mDayText = findViewById(R.id.xDay);
+        mSmokeFreeSince = findViewById(R.id.xSmokeFreeSince);
+        mMoneySaved = findViewById(R.id.xMoneySaved);
+        mLifeRegained = findViewById(R.id.xLifeRegained);
+        mCigNotSmoked = findViewById(R.id.xCigNotSmoked);
+        mTotalCigSmoked = findViewById(R.id.xTotalCigSmoked);
+        mMoneyWasted = findViewById(R.id.xMoneyWasted);
+        mLifeLost = findViewById(R.id.xLifeLost);
+
+        mProgressBar = findViewById(R.id.xMainProgressBar);
+        mProgressBar.setColor(ContextCompat.getColor(this, R.color.colorAccent));
+        mProgressBar.setBackgroundColor(ContextCompat.getColor(this, R.color.com_facebook_blue));
+        mProgressBar.setProgressBarWidth(getResources().getDimension(R.dimen.button_margin));
+        mProgressBar.setBackgroundProgressBarWidth(getResources().getDimension(R.dimen.button_margin));
+        int animationDuration = 2500; // 2500ms = 2,5s
+        mProgressBar.setProgressWithAnimation(1, animationDuration); // Default duration = 1500ms
+
+        Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+        int mSeconds = c.get(Calendar.SECOND);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        final int fixedTimeFromUSer = mDayFinal + (mHourFinal / 24) + (mMinuteFinal / (24 * 60));
+        final int tempTime = fixedTimeFromUSer + 1;
+        final int fCPD = CPD;
+        final float fYOS = YOS;
+        final float fPPP = PPP;
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (ProgressStatus <= 100) {
+                while (mProgressStatus <= 100) {
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
-                            mProgressBar.setProgress(ProgressStatus);
-                            mTextProgress.setText(ProgressStatus + " %");
+
+                            int currentTimeFromUser = mDay + (mHour / 24) + (mMinute / (24 * 60));
+                            mProgressStatus = (fixedTimeFromUSer / tempTime) * 100;
+                            mProgressBar.setProgress(mProgressStatus);
+                            mTextProgress.setText(mProgressStatus + " %");
+
+                            mSmokeFreeSince.setText((mDay - mDayFinal) + "d " + (mHour - mHourFinal) + "h " + (mMinute - mMinuteFinal) + "m");
+                            mMoneySaved.setText("$" + ((mDay - mDayFinal) * (int) fPPP));
+                            mLifeRegained.setText((mMinute - mMinuteFinal) * 275 + " mins");
+                            mCigNotSmoked.setText(String.valueOf(fCPD * (mDay - mDayFinal)));
+                            mTotalCigSmoked.setText(String.valueOf((int) (fCPD * fYOS * 365)) + "\n Cigarettes Smoked");
+                            mMoneyWasted.setText("$" + String.valueOf((int) (fPPP * (fYOS) * 365)) + "\n Money Wasted");
+                            mLifeLost.setText(String.valueOf(275 * fYOS) + " months \n Life Lost");
                         }
                     });
                     try {
-                        Thread.sleep(100);
+                        Thread.sleep(1);
 
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    ProgressStatus++;
+                    mTimeStamp++;
+                    if (mProgressStatus == 100) {
+                        dayNumber++;
+                        mDayText.setText("Day :" + dayNumber);
+                        mNumberofDays.setText("It's day number :" + dayNumber);
+                    }
                 }
             }
         }).start();
+
+        secondGridManager();
 
         mAuth = FirebaseAuth.getInstance();
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
@@ -93,13 +155,27 @@ public class MainActivity extends AppCompatActivity {
         mDatabase.keepSynced(true);
     }
 
+    private void secondGridManager() {
+        Date date = new Date(Long.parseLong(mTimeStamp.toString()));
+        mSmokeFreeSince.setText(dayNumber + "d");
+    }
+
     private void loadUserProfile() {
-        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("userdata", MODE_PRIVATE);
+        sharedPreferences = getApplicationContext().getSharedPreferences("userdata", MODE_PRIVATE);
         CPD = sharedPreferences.getInt("CPD", defaultValue);
         CPP = sharedPreferences.getInt("CPP", defaultValue);
         YOS = sharedPreferences.getFloat("YOS", defaultValue);
         PPP = sharedPreferences.getFloat("PPP", defaultValue);
         CUR = sharedPreferences.getString("CUR", null);
+        mYearFinal = sharedPreferences.getInt("YEAR", defaultValue);
+        mMonthFinal = sharedPreferences.getInt("MONTH", defaultValue);
+        mDayFinal = sharedPreferences.getInt("DAY", defaultValue);
+        mHourFinal = sharedPreferences.getInt("HOUR", defaultValue);
+        mMinuteFinal = sharedPreferences.getInt("MINUTE", defaultValue);
+        mTimeStamp = sharedPreferences.getLong("TIMESTAMP", defaultValue);
+        if (CPD == defaultValue) {
+            startActivity(new Intent(MainActivity.this, UserProfile.class));
+        }
     }
 
     @Override
@@ -177,7 +253,7 @@ public class MainActivity extends AppCompatActivity {
         builder.setPositiveButton("Yes, start over...", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                pickDateTime();
             }
         });
         builder.setNegativeButton("No, I can cope", new DialogInterface.OnClickListener() {
@@ -189,13 +265,75 @@ public class MainActivity extends AppCompatActivity {
         builder.show();
     }
 
+    private void pickDateTime() {
+        Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, MainActivity.this, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        mYearFinal = year;
+        mMonthFinal = month;
+        mDayFinal = dayOfMonth;
+
+        Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this,
+                MainActivity.this, mHour, mMinute, DateFormat.is24HourFormat(this));
+        timePickerDialog.show();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        mHourFinal = hourOfDay;
+        mMinuteFinal = minute;
+      /*  Toast.makeText(getApplicationContext(), "Year: " + mYearFinal + " \n" +
+                "Month: " + mMonthFinal + " \n" +
+                "Day: " + mDayFinal + " \n" +
+                "Hour: " + mHourFinal + " \n" +
+                "Minute: " + mMinuteFinal, Toast.LENGTH_LONG).show();*/
+    }
 
     private void logout() {
         FirebaseUser user = mAuth.getCurrentUser();
         do {
+            sharedPreferences.edit().clear().apply();
             FirebaseAuth.getInstance().signOut();
             mAuth.signOut();
         } while (user == null);
         finish();
     }
+
+//    public class CustomCountDownTimer extends CountDownTimer{
+//
+//        /**
+//         * @param millisInFuture    The number of millis in the future from the call
+//         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+//         *                          is called.
+//         * @param countDownInterval The interval along the way to receive
+//         *                          {@link #onTick(long)} callbacks.
+//         */
+//        public CustomCountDownTimer(long millisInFuture, long countDownInterval) {
+//            super(millisInFuture, countDownInterval);
+//        }
+//
+//        @Override
+//        public void onTick(long millisUntilFinished) {
+//            mProgressStatus = (int)(millisUntilFinished/100);
+//            mProgressBar.setProgress(mProgressStatus);
+//            mTextProgress.setText(mProgressStatus + " %");
+//        }
+//
+//        @Override
+//        public void onFinish() {
+//        //give notification of greetings
+//        }
+//    }
 }
--- app/src/main/java/com/shienh/rajbir/smokescreen/UserProfile.java
+++ app/src/main/java/com/shienh/rajbir/smokescreen/UserProfile.java
@@ -1,26 +1,39 @@
 package com.shienh.rajbir.smokescreen;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class UserProfile extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class UserProfile extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private EditText mCigPerDay, mCigPerPack, mYearsOfSmoking, mPricePerPack;
     private Spinner mCurrencyType;
     private String currency = null;
     private LinearLayout mBaseView;
 
-    private Button mSaveData;
+    private Button mSaveData, mDateTimePicker;
+
+    private int mDay, mMonth, mYear, mHour, mMinute;
+    private int mDayFinal, mMonthFinal, mYearFinal, mHourFinal, mMinuteFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +59,14 @@ public class UserProfile extends AppCompatActivity {
             }
         });
 
+        mDateTimePicker = findViewById(R.id.xUserDateTimePicker);
+        mDateTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickDateTime();
+            }
+        });
+
         mSaveData = findViewById(R.id.xUserSave);
         mSaveData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,13 +84,14 @@ public class UserProfile extends AppCompatActivity {
                 } else if (TextUtils.isEmpty(tempppp)) {
                     Snackbar.make(mBaseView, "Price per pack cannot be zero", 3000).show();
                 } else {
-
                     int cpd = Integer.parseInt(tempcpd);
                     int cpp = Integer.parseInt(tempcpp);
                     Float yos = Float.parseFloat(tempyos);
                     Float ppp = Float.parseFloat(tempppp);
                     String fCurrency = currency;
-                    if ((cpd > 0) && (cpp > 0) && (yos > 0) && (ppp > 0) && (fCurrency != null)) {
+                    // Long tsLong = System.currentTimeMillis()/1000;
+                    if ((cpd > 0) && (cpp > 0) && (yos > 0) && (ppp > 0) && (fCurrency != null) &&
+                            (mYearFinal > 0) && (mMonthFinal > 0) && (mDayFinal > 0)) {
                         SharedPreferences sharedPreferences = getSharedPreferences("userdata", Context.MODE_PRIVATE);
                         SharedPreferences.Editor editor = sharedPreferences.edit();
                         editor.putInt("CPD", cpd);
@@ -77,7 +99,16 @@ public class UserProfile extends AppCompatActivity {
                         editor.putFloat("YOS", yos);
                         editor.putFloat("PPP", ppp);
                         editor.putString("CUR", fCurrency);
+                        editor.putInt("YEAR", mYearFinal);
+                        editor.putInt("MONTH", mMonthFinal);
+                        editor.putInt("DAY", mDayFinal);
+                        editor.putInt("HOUR", mHourFinal);
+                        editor.putInt("MINUTE", mMinuteFinal);
                         editor.apply();
+                        Intent loginIntent = new Intent(UserProfile.this, MainActivity.class);
+                        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(loginIntent);
+                        Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
                     } else {
                         Snackbar.make(mBaseView, "Make sure all values are correct, not Zero or negative", 3000).show();
                     }
@@ -86,4 +117,46 @@ public class UserProfile extends AppCompatActivity {
         });
     }
 
+    private void pickDateTime() {
+        Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(UserProfile.this, UserProfile.this, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        mYearFinal = year;
+        mMonthFinal = month;
+        mDayFinal = dayOfMonth;
+
+        Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(UserProfile.this,
+                UserProfile.this, mHour, mMinute, DateFormat.is24HourFormat(this));
+        timePickerDialog.show();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        mHourFinal = hourOfDay;
+        mMinuteFinal = minute;
+        Calendar calendar = new GregorianCalendar(mYearFinal, mMonthFinal, mDayFinal, mHourFinal, mMinuteFinal);
+        long finalTimeStamp = calendar.getTimeInMillis() / 1000;
+        SharedPreferences sharedPreferences = getSharedPreferences("userdata", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putLong("TIMESTAMP", finalTimeStamp);
+        //  Toast.makeText(getApplicationContext(),String.valueOf(finalTimeStamp),Toast.LENGTH_LONG).show();
+        editor.apply();
+ /*       Toast.makeText(getApplicationContext(), "Year: " + mYearFinal + " \n" +
+                "Month: " + mMonthFinal + " \n" +
+                "Day: " + mDayFinal + " \n" +
+                "Hour: " + mHourFinal + " \n" +
+                "Minute: " + mMinuteFinal, Toast.LENGTH_LONG).show();*/
+    }
 }
